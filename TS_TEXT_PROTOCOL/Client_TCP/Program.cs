82ace828680f6f56e6364db4fb4673ffcf4bea64
Krzysztof <czarneckikrzysztof666@gmail.com>
2018-11-23 08:51:28 +0100

    Very first commit

---
+++ TS_TEXT_PROTOCOL/Client_TCP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Client_TCP
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
