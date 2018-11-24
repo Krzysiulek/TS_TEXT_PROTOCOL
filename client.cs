e6a81c51e29d73364b9885faf0d5aa2a38802e54
Błażej <44238271+MamoToJaa@users.noreply.github.com>
2018-11-24 19:26:47 +0100

    Update client.cs

--- client.cs
+++ client.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -9,6 +9,7 @@ namespace client_tcp
     {
         private readonly Int32 port;
         private readonly string IP;
+        private NetworkStream serverStream;
         public TcpClient client;
 
         public Client(string IP_, Int32 port_)
@@ -21,12 +22,34 @@ namespace client_tcp
 
         public void Send()
         {
-
-            NetworkStream serverStream = client.GetStream();
+            Console.WriteLine("Write your message: ");
+            serverStream = client.GetStream();
             byte[] outStream = Encoding.ASCII.GetBytes(Console.ReadLine());
             serverStream.Write(outStream, 0, outStream.Length);
             Console.WriteLine("Size: {0}", outStream.Length);
             serverStream.Flush();
+        }
+
+        public void Receive()
+        {
+            try
+            {
+                serverStream = client.GetStream();
+                byte[] receivedData = new byte[4096];
+
+                if (serverStream.DataAvailable)
+                {
+                    serverStream.Read(receivedData, 0, receivedData.Length);
+                    String text = Encoding.ASCII.GetString(receivedData); //odebrany tekst od klienta
+                    text = text.Substring(0, text.IndexOf('\0'));
+                    Console.WriteLine("Text from server: {0}", text);
+                    serverStream.Flush();
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
         }
     }
 }
