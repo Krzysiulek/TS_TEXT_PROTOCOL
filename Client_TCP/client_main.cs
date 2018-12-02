c26960b1cd997f9c7516b29ae717b3f116ac44eb
Krzysztof <czarneckikrzysztof666@gmail.com>
2018-12-02 19:38:26 +0100

    Update client_main.cs

--- Client_TCP/client_main.cs
+++ Client_TCP/client_main.cs
@@ -6,39 +6,17 @@ namespace client_tcp
     class MainClass
     {
         private static readonly Int32 port = 8080;
-        private static readonly string IP = "127.0.0.1";
-        //private static readonly string IP = "192.168.43.32";
+        //private static readonly string IP = "127.0.0.1";
+        private static readonly string IP = "192.168.0.24";
 
       
         public static void Main(string[] args)
         {
-            string tmp2 = DataOperations.SetData("addID", "cor", 123, 32);
-            string tmp = DataOperations.SetData("addID", "cor", 123, -12, -195, 197, "1234");
-            Console.WriteLine("OP: {0}\n" +
-                              "ST: {1}\n" +
-                              "ID: {2}\n" +
-                              "TS: {3}\n" +
-                              "A1: {4}\n" +
-                              "A2: {5}\n" +
-                              "IO: {6}\n" +
-                              "DT: {7}\n",
-                              DataOperations.GetOP(tmp),
-                              DataOperations.GetST(tmp),
-                              DataOperations.GetID(tmp),
-                              DataOperations.GetTS(tmp),
-                              DataOperations.GetA1(tmp),
-                              DataOperations.GetA2(tmp),
-                              DataOperations.GetIO(tmp),
-                              DataOperations.GetDT(tmp)
-                                                  );
-
-
-
             Client client = new Client(IP, port);
             Thread receive = new Thread(client.Receive);
             receive.Start();
             client.Send();
-            Thread.Sleep(2000);
+            Thread.Sleep(2000); //przeczekanie aż klient odbierze ID
 
             while(true)
             {
